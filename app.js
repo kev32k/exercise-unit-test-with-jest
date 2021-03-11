c6157321aef18480cab35f643af0a5462c48731e
kev32k <lealjosue@icloud.com>
2021-03-11 21:13:03 +0000

    tome pal pinto

---
+++ app.js
@@ -0,0 +1,31 @@
+//console.log('Hello World!');
+
+const sum = (a,b) => {
+    return a + b;
+}
+
+console.log(sum(7,3));
+
+const fromEuroToDollar = euro => {
+    return euro * 1.206;
+}
+
+console.log(fromEuroToDollar(120));
+
+
+const fromDollarToYen = dollar => {
+    //First convert dollar to Euro.
+    return (dollar * 0.83) * 127.9;
+}
+
+console.log(fromDollarToYen(100));
+
+const fromYenToPound = yen => {
+    //First convert Yen to Euro.
+    return (yen * 0.0078) * 0.8;
+}
+
+console.log(fromYenToPound(100));
+
+
+module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
